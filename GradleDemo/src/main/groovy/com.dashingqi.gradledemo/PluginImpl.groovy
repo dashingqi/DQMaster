916a53b078edfb8732de693f5472ff6d7617d2ac
DashingQi <m13478493231@163.com>
2020-08-10 17:15:33 +0800

    [publish plugin]

--- GradleDemo/src/main/groovy/com.dashingqi.gradledemo/PluginImpl.groovy
+++ GradleDemo/src/main/groovy/com.dashingqi.gradledemo/PluginImpl.groovy
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 
 public class PluginImpl implements Plugin<Project> {
     void apply(Project project) {
-        project.task('testTask') << {
+        project.task('testTask') {
             println "Hello gradle plugin"
         }
     }
---
+++ module-paging/src/main/java/com/dashingqi/module/paging/positional/MoviePositionalDataSource.kt
@@ -0,0 +1,9 @@
+package com.dashingqi.module.paging.positional
+
+/**
+ * @author : zhangqi
+ * @time : 2020/8/10
+ * desc :
+ */
+class MoviePositionalDataSource {
+}
