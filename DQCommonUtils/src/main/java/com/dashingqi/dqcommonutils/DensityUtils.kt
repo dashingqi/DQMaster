d5447faa032982c6378e01a58029063079a8abf3
DashingQi <m13478493231@163.com>
2020-08-25 17:00:43 +0800

    [update]

---
+++ DQCommonUtils/src/main/java/com/dashingqi/dqcommonutils/DensityUtils.kt
@@ -0,0 +1,53 @@
+package com.dashingqi.dqcommonutils
+
+import android.content.Context
+
+/**
+ * @author : zhangqi
+ * @time : 2020/8/25
+ * desc : 手机屏幕工具类
+ */
+object DensityUtils {
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    fun dip2pxInt(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    fun px2dipInt(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    fun dip2px(context: Context, dpValue: Float): Float {
+        val scale = context.resources.displayMetrics.density
+        return dpValue * scale
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    fun px2dip(context: Context, pxValue: Float): Float {
+        val scale = context.resources.displayMetrics.density
+        return pxValue / scale
+    }
+
+    fun getScreenWidthPixels(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.widthPixels
+    }
+
+    fun getScreenHeightPixels(context: Context): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return displayMetrics.heightPixels
+    }
+}
