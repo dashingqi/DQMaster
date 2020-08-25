8fd71e1da5f77125466281996ece8f58de401bea
DashingQi <m13478493231@163.com>
2020-08-25 15:43:19 +0800

    [update]

---
+++ module-3dtouch/src/main/java/com/chiatai/module_3dtouch/ThreeDTouchActivity.kt
@@ -0,0 +1,11 @@
+package com.chiatai.module_3dtouch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThreeDTouchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three_d_touch)
+    }
+}
